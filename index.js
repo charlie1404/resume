501675aa3eb6237d7e5029c90d28f7b0209c5ec9
Shailendra Gupta <guptas199614@gmail.com>
2022-12-06 22:54:55 +0530

    use react + vite

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const fs = require('fs');
-const http = require('http');
 const core = require('@actions/core');
 const puppeteer = require('puppeteer');
+const { build, preview } = require('vite');
 
 let server = null;
 let browser = null;
 
 async function run() {
-  const inputFilePath = './resume.html';
-  const outputFilePath = './resume.pdf';
+  const outputFilePath = './dist/resume.pdf';
 
-  core.info('Starting HTTP server for file!!');
-  server = http
-    .createServer((req, res) => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(fs.readFileSync(inputFilePath).toString());
-    })
-    .listen(8088);
+  core.info('Starting vite server!!');
+
+  await build();
+  server = await preview({ preview: { port: 8088 } });
 
   core.info('Starting puppeteer!!');
   browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-gpu'] });
@@ -28,13 +23,13 @@ async function run() {
   core.info('Starting generating PDF!!');
   await page.pdf({
     path: outputFilePath,
-    format: 'A4',
+    format: 'Legal',
     margin: { top: '0', left: '0', right: '0', bottom: '0' },
     printBackground: true,
   });
 
   core.info('PDF generation complete!!');
-  server.close();
+  server.httpServer.close();
   browser.close();
 }
 
@@ -42,7 +37,7 @@ run().catch((error) => {
   core.error(error.message);
   core.setFailed('Action Failed');
   if (server) {
-    server.close();
+    server.httpServer.close();
   }
   if (browser) {
     browser.close();
